d925811ffb67c4aedb5ee4f50a6f3e1543b1a4e8
Stas Kudinov <dedbanzai91@mail.ru>
2022-06-18 01:36:33 +0300

    hotfixed 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
-const likeButton = document.querySelector('.elements__button');
+// const likeButton = document.querySelector('.elements__button');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__field_name');
+let jobInput = document.querySelector('.popup__field_job');
 let profileName = document.querySelector('.profile__name');
 let profilejob = document.querySelector('.profile__job');
 
@@ -24,10 +24,10 @@ function popupAreaClose(evt) {
   };
 };
 
-function likeActive(evt) {
-  evt.preventDefault(evt);
-  likeButton.classList.toggle('elements__button_active');
-};
+// function likeActive(evt) {
+//   evt.preventDefault(evt);
+//   likeButton.classList.toggle('elements__button_active');
+// };
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -39,5 +39,5 @@ function formSubmitHandler(evt) {
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
 popup.addEventListener('click', popupAreaClose);
-likeButton.addEventListener('click', likeActive);
+// likeButton.addEventListener('click', likeActive);
 formElement.addEventListener('submit', formSubmitHandler);
