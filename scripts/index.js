9d8a6a23316d1a7eb63bf442361f5bfb6b899252
Stas Kudinov <dedbanzai91@mail.ru>
2022-06-29 15:39:41 +0300

    feat: add render cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,57 @@
-const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.popup__close-button');
-// const likeButton = document.querySelector('.elements__button');
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__field_name');
-let jobInput = document.querySelector('.popup__field_job');
-let profileName = document.querySelector('.profile__name');
-let profilejob = document.querySelector('.profile__job');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsContainer = document.querySelector('.elements');
+
+const renderCards = () => {
+  initialCards.forEach(renderCard);
+};
+
+const renderCard = (element) => {
+  const cardTemplate = document.querySelector('.card-template').content;
+  const elementsCard = cardTemplate.querySelector('.elements__card').cloneNode(true);
+  elementsCard.querySelector('.elements__image').src = element.link;
+  elementsCard.querySelector('.elements__image').alt = element.name;
+  elementsCard.querySelector('.elements__name').textContent = element.name;
+
+  cardsContainer.append(elementsCard);
+};
+
+ renderCards();
+
+ const editButton = document.querySelector('.profile__edit-button');
+ const popup = document.querySelector('.popup');
+ const closeButton = document.querySelector('.popup__close-button');
+ // const likeButton = document.querySelector('.elements__button');
+ let formElement = document.querySelector('.popup__container');
+ let nameInput = document.querySelector('.popup__field_name');
+ let jobInput = document.querySelector('.popup__field_job');
+ let profileName = document.querySelector('.profile__name');
+ let profilejob = document.querySelector('.profile__job');
 
 function popupOpen(evt) {
   evt.preventDefault();
@@ -25,7 +70,7 @@ function popupAreaClose(evt) {
 };
 
 // function likeActive(evt) {
-//   evt.preventDefault(evt);
+//   evt.preventDefault();
 //   likeButton.classList.toggle('elements__button_active');
 // };
 
